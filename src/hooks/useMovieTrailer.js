b3c79cae1e76b9faa2fecf6228679dd128cd96e3
Shubham mittal <shubmittal.92@gmail.com>
2025-01-07 19:58:45 +0530

    updt

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
-=======
-import { useEffect } from "react";
->>>>>>> a296deaec28a3f2ea500cfb9eadafc4420faf1b9
 import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/moviesSlice";
 
 const useMovieTrailer = (movieId) => {
-<<<<<<< HEAD
   const [error, setError] = useState(false);
   const dispatch = useDispatch();
   const trailerVideo = useSelector((store) => store.movies.trailerVideo);
@@ -16,19 +11,10 @@ const useMovieTrailer = (movieId) => {
   const getMovieVideos = async () => {
     try {
       const data = await fetch(
-=======
-  const dispatch = useDispatch();
-
-  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
-
-  const getMovieVideos = async () => {
-    const data = await fetch(
->>>>>>> a296deaec28a3f2ea500cfb9eadafc4420faf1b9
       "https://api.themoviedb.org/3/movie/" +
         movieId +
         "/videos?language=en-US",
       API_OPTIONS
-<<<<<<< HEAD
       );
       if (!data.ok) throw new Error("Failed to fetch");
       const json = await data.json();
@@ -48,18 +34,3 @@ const useMovieTrailer = (movieId) => {
 };
 
 export default useMovieTrailer;
-=======
-    );
-    const json = await data.json();
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    dispatch(addTrailerVideo(trailer));
-  };
-  useEffect(() => {
-    !trailerVideo && getMovieVideos();
-  }, []);
-};
-
-export default useMovieTrailer;
->>>>>>> a296deaec28a3f2ea500cfb9eadafc4420faf1b9
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,7 +9,6 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: "AIzaSyAQFdgTEzc7VkXUHF0e7AP0-E4kx0NTGf4",
   authDomain: "netflixgpt-b7165.firebaseapp.com",
   projectId: "netflixgpt-b7165",
@@ -17,15 +16,6 @@ const firebaseConfig = {
   messagingSenderId: "315976586776",
   appId: "1:315976586776:web:fc93b5c3aead7c6653556d",
   measurementId: "G-2FJ0X9Z1Q2"
-=======
-  apiKey: "AIzaSyAwKbz6Wo1UVp1BzKSJVXV6hTz3vwy74k8",
-  authDomain: "netflixgpt-57a56.firebaseapp.com",
-  projectId: "netflixgpt-57a56",
-  storageBucket: "netflixgpt-57a56.appspot.com",
-  messagingSenderId: "1093133802935",
-  appId: "1:1093133802935:web:c284961959cce2a54adc32",
-  measurementId: "G-DEHFXGZ999",
->>>>>>> a296deaec28a3f2ea500cfb9eadafc4420faf1b9
 };
 
 // Initialize Firebase
