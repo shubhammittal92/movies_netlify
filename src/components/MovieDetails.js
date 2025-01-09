6a7554c395382c60e185ffe7e4b2a5e57aebde89
Shubham mittal <93783550+shubhammittal92@users.noreply.github.com>
2025-01-10 00:24:28 +0530

    Add files via upload

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,17 +1,12 @@
-// import { useParams } from "react-router";
+import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { API_OPTIONS, IMG_CDN_URL } from "../utils/constants";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 
-import { useDispatch } from "react-redux";
-
-const MovieDetails= (movieId) => {
-    console.log(movieId,"fd")
-//   const { movieId } = useParams();
-  const dispatch = useDispatch();
+const MovieDetailsPage = () => {
+  const { movieId } = useParams();
   const [movies, setMovies] = useState(null);
   const [video, setVideo] = useState(null);
+
   useEffect(() => {
     fetchMovies();
     fetchMovieVideo();
@@ -20,11 +15,10 @@ const MovieDetails= (movieId) => {
   const fetchMovies = async () => {
     try {
       const data = await fetch(
-        "https://api.themoviedb.org/3/movie/" + movieId + "?language=en-US",
+        `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
         API_OPTIONS
       );
       const json = await data.json();
-      console.log(json);
       setMovies(json);
     } catch (error) {
       console.log(error);
@@ -34,127 +28,87 @@ const MovieDetails= (movieId) => {
   const fetchMovieVideo = async () => {
     try {
       const data = await fetch(
-        "https://api.themoviedb.org/3/movie/" +
-          movieId +
-          "/videos?language=en-US",
+        `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
         API_OPTIONS
       );
       const json = await data.json();
-      console.log(json);
 
-      const filterData = json.results.filter(
-        (video) =>
-          video.type === "Trailer" ||
-          video.type === "Official Trailer" ||
-          video.type === "Teaser" ||
-          video.type === "Featurette" ||
-          video.type.toLowerCase().includes("trailer")
+      const filterData = json.results.find((video) =>
+        ["Trailer", "Official Trailer", "Teaser"].includes(video.type)
       );
 
-      console.log(filterData);
-
-      setVideo(json.results[0]);
-      // setVideo(filterData);
+      setVideo(filterData || json.results[0]);
     } catch (error) {
       console.log(error);
     }
   };
 
+  if (!movies || !video) return <div>Loading...</div>;
+
   return (
     <>
-      {/* DESKTOP VIEW */}
-      <div className='hidden md:mt-[10px]  md:block'>
-        {/* <div className='flex flex-col'>
-          <p className='text-4xl text-center mt-8 font-bold text-teal-200'>
-            {movies?.title}
-          </p>
-          <img
-            className=' w-[90%] border-[1px] border-brand-beige mt-8 mx-auto rounded-xl'
-            src={IMG_CDN_URL + movies?.poster_path}
-            alt='movie poster'
-          />
-        </div>
-
-        <div className='flex mt-[75px] flex-col'>
-          <p className='text-sm text-left px-4 py-2 w-[60%] ml-5 mt-8  border-[1px] border-teal-200 rounded-lg text-white'>
-            {movies?.overview}
-          </p>
-          <div className='flex ml-[10px] mt-6'>
-            {movies?.genres?.map((genre) => {
-              return (
-                <p
-                  key={genre?.id}
-                  className='rounded-full ml-2 px-4 py-2  border-[1px] border-teal-200 text-white'>
-                  {genre?.name}
-                </p>
-              );
-            })}
-          </div>
-
-          <p className='text-xl text-left ml-5 mt-8 px-4 py-2 w-[60%] rounded-lg border-[1px] border-teal-200 text-white'>{movies?.status} &nbsp; {movies?.release_date}</p>
-        </div>  */}
-        {/* <Navbar /> */}
-
-        <div className='w-screen -mt-24 absolute aspect-video hidden md:block'>
+      <div className="hidden md:block bg-black text-white">
+        <div className="relative w-full h-screen">
+          {/* Video Background */}
           <iframe
-            className=''
-            width='100%'
-            height='100%'
-            src={
-              "https://www.youtube.com/embed/" +
-              video?.key +
-              "?autoplay=1&mute=1"
-            } //
-            title='YouTube video player'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            allowFullscreen></iframe>
-        </div>
-      </div>
-      <div className='absolute hidden md:block top-[0px] h-screen w-screen bg-gradient-to-r from-black'>
-        <div className='text-white absolute ml-14 top-[250px] '>
-          <h1 className='font-semiboldbold text-6xl'>{movies?.title}</h1>
-          <p className='mt-2 w-[60%]'>{movies?.overview}</p>
-          <div className='flex ml-[-15px] mt-6'>
-            {movies?.genres?.map((genre) => {
-              return (
-                <p
-                  key={genre?.id}
-                  className='rounded-full ml-2 px-4 py-2 bg-brand-charcoal text-white'>
-                  {genre?.name}
-                </p>
-              );
-            })}
+            className="absolute top-0 left-0 w-full h-full z-0"
+            src={`https://www.youtube.com/embed/${video?.key}?autoplay=1&mute=1`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+
+          {/* Movie Information */}
+          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black/60 via-black/40 to-black/80 z-10 flex flex-col justify-center items-start px-10">
+            <h1 className="text-6xl font-bold text-white">{movies?.title}</h1>
+            <p className="mt-4 text-lg text-white">{movies?.overview}</p>
+            <div className="flex mt-4">
+              {movies?.genres?.map((genre) => (
+                <span
+                  key={genre.id}
+                  className="rounded-full bg-teal-500 text-white px-4 py-2 mr-2"
+                >
+                  {genre.name}
+                </span>
+              ))}
+            </div>
+            <p className="mt-4">
+              <strong>Status:</strong> {movies?.status} <br />
+              <strong>Release Date:</strong> {movies?.release_date}
+            </p>
           </div>
         </div>
       </div>
 
-      {/* MOBILE VIEW */}
-      <div className='md:hidden '>
+      {/* Mobile View */}
+      <div className="md:hidden bg-black text-white">
         <img
-          className=' w-[90%] border-[1px] border-brand-beige mt-8 mx-auto rounded-xl'
+          className="w-[90%] mx-auto mt-8 rounded-xl"
           src={IMG_CDN_URL + movies?.poster_path}
-          alt='movie poster'
+          alt="Movie Poster"
         />
-
-        <p className='text-sm text-left px-4 py-2 w-[90%] ml-5 mt-8  border-[1px] border-teal-200 rounded-lg text-white'>
-          {movies?.overview}
-        </p>
-        <div className='flex flex-wrap left-4 absolute top-5  mt-6'>
-          {movies?.genres?.map((genre) => {
-            return (
-              <p
-                key={genre?.id}
-                className='rounded-full m-2 px-4 py-2 bg-black text-white'>
-                {genre?.name}
-              </p>
-            );
-          })}
+        <div className="mt-4 px-4">
+          <h1 className="text-3xl font-bold">{movies?.title}</h1>
+          <p className="mt-4">{movies?.overview}</p>
+          <div className="flex flex-wrap mt-4">
+            {movies?.genres?.map((genre) => (
+              <span
+                key={genre.id}
+                className="rounded-full bg-teal-500 text-white px-4 py-2 mr-2 mb-2"
+              >
+                {genre.name}
+              </span>
+            ))}
+          </div>
+          <p className="mt-4">
+            <strong>Status:</strong> {movies?.status} <br />
+            <strong>Release Date:</strong> {movies?.release_date}
+          </p>
         </div>
       </div>
-
-      {/* <Footer /> */}
     </>
   );
 };
 
-export default MovieDetails;
+export default MovieDetailsPage;
